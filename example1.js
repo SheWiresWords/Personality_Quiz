05c1ea64ee4a78b850e09e48c7616a2f70a936b0
Alexa Curtis <142436963+alexaycurtis@users.noreply.github.com>
2025-07-15 00:47:37 +0000

    comit

--- example1.js
+++ example1.js
@@ -1,7 +1,8 @@
-var pythonScore = 0
-var javaScore = 0
-
+var pythonScore = 0;
+var javaScore = 0;
 var counter=0;
+var result = document.getElementId("reset")
+
 
 //buttons
 var q1a1 = document.getElementById("q1a1");
@@ -13,10 +14,46 @@ var q2a2 = document.getElementById("q2a2");
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 
+q1a1.addEventListener("click", java);
+q1a2.addEventListener("click", python);
 
-var restart = 0;
+q2a1.addEventListener("click", java);
+q2a2.addEventListener("click", python);
 
-var result = 0;
+q3a1.addEventListener("click", python);
+q3a2.addEventListener("click", java);
+
+function java(){
+   javaScore++;
+   questionCount++;
+}
+
+if(questionCount == 3){
+   console.log("Quiz over");
+   updateResult();
+}
+
+function python(){
+   pythonScore++;
+   questionCount++;
+}
+
+if(questionCount == 3){
+   console.log("the quiz is done");
+   updateResult();
+}
+
+
+function updateResult() {
+   if(pythonScore >= 2){
+      console.log("You are a Python person!");
+      result.innerHTML("You are a Python person!")
+   }
+   else{
+      console.log("You are a Java person!");
+      result.innerHTML("You are a Java person!")
+   }
+}
 
 /* Document is a DOM Object 
    Get element by id is a method
