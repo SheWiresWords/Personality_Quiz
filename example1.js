1bd9e124aca1d9f351e0ffe50f00506fd02fad17
Alexa Curtis <142436963+alexaycurtis@users.noreply.github.com>
2025-07-15 00:11:29 +0000

    commit

--- example1.js
+++ example1.js
@@ -3,7 +3,22 @@ var javaScore = 0
 
 var counter=0;
 
+//buttons
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+
+
 var restart = 0;
 
 var result = 0;
 
+/* Document is a DOM Object 
+   Get element by id is a method
+   element.interHTML accesses the property innerHTML
+*/
