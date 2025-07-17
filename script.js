fe6413d34bbab841c0c3205437bf08235c330b49
Alexa Curtis <142436963+alexaycurtis@users.noreply.github.com>
2025-07-17 16:29:57 +0000

    git commit

--- script.js
+++ script.js
@@ -211,4 +211,22 @@ function resetFunction(){
 /* Document is a DOM Object 
    Get element by id is a method
    element.interHTML accesses the property innerHTML
-*/
+ahhh
+im acctually going
+to start
+taking
+notes
+in here
+what the freak
+   */
+
+ahh
+
+bro dawg
+i
+am
+BATMAN
+yes
+lol
+haha
+ALFRED!
